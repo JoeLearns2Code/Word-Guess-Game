153b0c9e8927b7a61c2d4fa3ccd3a3f4eb8a5c79
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-05-29 23:08:26 -0700

    preliminary work on HTML and JS files.

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+//Array of words to guess
+
+var characters = ['luke skywalker', 'han solo', 'princess leia', 'lando', 'yoda', 'palpatine', 'darth vader', 'darth maul', 'count dooku', 'kylo ren', 'rey', 'chewbacca', 'jar jar', 'qui gon jinn', 'obiwan kenobi' ];
+
+//random word selected
+var chosenWord = characters[Math.floor(Math.random() * characters.length)];
+console.log(chosenWord);
+
+//array of player's selected letters
+
+var playerGuess = []
+
+//wins accrued
+
+var wins = 0
+
+//remaining guesses
+
+var guessCount = 15
+
+
+
+//starting state of game
+
+function openGame () {
+   alert('welcome')
+   document.getElementById("wins").textContent = wins;
+   document.getElementById("guessesLeft").textContent = guessCount;
+   document.getElementById("alreadyGuessed").textContent = playerGuess;
+
+}
+
+window.onload = openGame;
+
+//press a key to start
+
+
+
+//display underscores for selected word
+
+
+
+//if player selects a letter in the word: replace underscore with letter, add letter to player's Already Guessed array table, subtract from Guesses Remaining.  
+
+
+//if player does not select a letter in the word: add letter to player's Already Guess array table, subtract from Guesses Remaining.
+
+
+
+//if player completely matches the word: add 1 to Wins, reset Guesses Remaining and Letters Already Guessed, display character image, select a new word.
+
+
+//if player runs out of Guesses Remaining: game over
